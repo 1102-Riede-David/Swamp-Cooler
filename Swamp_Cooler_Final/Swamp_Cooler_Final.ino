3eb8b86167fae9216737523f51ea4c20d2aa2dca
1102-Riede-David <113651693+1102-Riede-David@users.noreply.github.com>
2024-05-13 01:47:12 -0700

    Add files via upload
    Updated

--- Swamp_Cooler_Final/Swamp_Cooler_Final.ino
+++ Swamp_Cooler_Final/Swamp_Cooler_Final.ino
@@ -114,21 +114,6 @@ void setup() {
 
   myStepper.setSpeed(motorSpeed);
   
-  
-  #ifndef ESP8266
-  while (!Serial); // wait for serial port to connect. Needed for native USB
-  #endif
-
-  if (! rtc.begin()) {
-    Serial.println("Couldn't find RTC");
-    Serial.flush();
-    while (1) delay(10);
-  }
-
-  if (! rtc.isrunning()) {
-    Serial.println("RTC is NOT running, let's set the time!");
-    rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
-  }
 
 
   lcd.begin(16, 2);
